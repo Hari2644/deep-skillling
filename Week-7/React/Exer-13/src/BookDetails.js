307464eb292d2e6c4d7d3899a78a85b03f89ec6e
Hari2644 <152772370+Hari2644@users.noreply.github.com>
2025-08-03 23:05:07 +0530

    Create BookDetails.js

---
+++ Week-7/React/Exer-13/src/BookDetails.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function BookDetails() {
+  const books = [
+    { id: 1, title: "Atomic Habits", author: "James Clear" },
+    { id: 2, title: "Deep Work", author: "Cal Newport" },
+    { id: 3, title: "The Alchemist", author: "Paulo Coelho" }
+  ];
+
+  return (
+    <div>
+      <h2>📚 Book Details</h2>
+      <ul>
+        {books.map(book => (
+          <li key={book.id}>{book.title} - {book.author}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default BookDetails;
